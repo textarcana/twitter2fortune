72a8e3975b2cc1d689a672a37aff5e553209171f
Thomas Menari <t@menari.eu>
2012-02-10 12:52:01 +0000

    a work in progress to use sqlite db

--- convert.rb
+++ convert.rb
@@ -1,26 +1,31 @@
 require 'rubygems'
 require 'twitter'
+require 'sqlite3'
 
-User = 'devops_borat'
-Output = File.open(User, 'w')
+User = 'bbcnews'
 
-last_id = 0
+db = SQLite3::Database.new("twitter2fortune.db")
+db.execute("CREATE TABLE IF NOT EXISTS tweets(user TEXT, id INTEGER, tweet TEXT, PRIMARY KEY(user, id))");
+
+last_id = db.get_first_value("SELECT id from tweets WHERE user=? ORDER BY id DESC LIMIT 1", User)
+max_id = 0 
 
 while
-  begin 
-    if last_id > 0 then
-      timeline = Twitter.user_timeline(User, :max_id => last_id, :count => 200, :exclude_replies => 1, :trim_user => 1)
+  begin
+    puts "loop! last-id: #{last_id} max-id: #{max_id}" 
+    if max_id > 0
+      timeline = Twitter.user_timeline(User, :since_id => last_id, :max_id => max_id, :count => 200, :exclude_replies => 1, :trim_user => 1)
     else
-      timeline = Twitter.user_timeline(User, :count => 200, :exclude_replies => 1, :trim_user => 1)
+      timeline = Twitter.user_timeline(User, :since_id => last_id, :count => 200, :exclude_replies => 1, :trim_user => 1)
     end
   rescue Twitter::Error::BadGateway
     retry
   end
 
   break if timeline.empty?
-  last_id = timeline.last.id - 1
+  max_id = timeline.last.id - 1
   puts "Recieved #{timeline.size} tweets below index #{last_id}"
   timeline.each do |st|
-    Output.write("#{st.text}\n%\n")
+    db.execute("INSERT into tweets VALUES (?, ?, ?)", User, st.id, st.text); 
   end
 end
